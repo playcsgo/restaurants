ea7d2ddbd4db3b8afcca5a480b178308abbc7df2
playcsgo <sakura_0125@hotmail.com>
2024-03-06 15:39:11 +0800

    feat: add signUp page

---
+++ controllers/user-controler.js
@@ -0,0 +1,24 @@
+const db = require('../models')
+const { User } = db
+const bcrypt = require('bcryptjs')
+
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  },
+  signUp: (req, res) => {
+    const { name, email, password } = req.body
+    bcrypt.hash(password, 10)
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => {
+        res.redirect('/signin')
+      })
+      .catch(err => console.log(err))
+  }
+}
+
+module.exports = userController  
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants', adminController.getRestaurants);
+router.get('/restaurants', adminController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
