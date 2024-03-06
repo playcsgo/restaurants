c85495e10f810902d4692d1657d2920da25e3154
playcsgo <sakura_0125@hotmail.com>
2024-03-06 14:47:54 +0800

    feat: sequelize init

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const routes = require('./routes')
 const {engine} = require('express-handlebars')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+const db = require('./models')
 
 app.engine('hbs', engine({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
