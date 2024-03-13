c78ecdbab1635812453aaa870a262c0c8e9860f4
playcsgo <sakura_0125@hotmail.com>
2024-03-13 16:31:46 +0800

    feat: add category id column

---
+++ migrations/20240313082332-add-category-id-to-restaurant.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { QueryError } = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'category_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      reference: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'category_id')
+  }
+};
