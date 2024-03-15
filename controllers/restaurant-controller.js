fd0dc8d04c27258bc89b02a2f7a28b76c42911ad
playcsgo <sakura_0125@hotmail.com>
2024-03-15 21:22:16 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,18 @@
+const { Restaurant, Category } = require('../models')
+
 restaurantControllers = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      include: Category,
+      raw: true,
+      nest: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 20)
+      }))
+      return res.render('restaurants', {restaurants: data})
+    })
   }
 }
 
