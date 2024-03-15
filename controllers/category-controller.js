903a2e53a80d32609bed7b3bcc9fd701e93292d7
playcsgo <sakura_0125@hotmail.com>
2024-03-15 20:56:20 +0800

    feat: admin-category-CRUD

---
+++ controllers/category-controller.js
@@ -0,0 +1,43 @@
+const { Restaurant, Category } = require('../models')
+
+const categoryController = {
+  getCategories: (req, res, next) => {
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/categories', { categories, category })
+      })
+      .catch(err => next(err))
+  },
+  postCategories: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+   return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  putCategories: (req, res, next) => {
+    const { name } = req.body
+    if(!name) throw new Error('Category name is required')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error('Category does not exist!')
+        return Category.update({ name }, { where: { id: req.params.id } })
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  deleteCategories: (req, res, next) => {
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error('Category does not exist!')
+        return category.destroy()
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  }
+}
+
+module.exports = categoryController
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -2,6 +2,7 @@ const dayjs = require('dayjs')
 
 module.exports = {
   currentYear: () => dayjs().year(),
+
   ifCond: function (a, b, option) {
     return a === b ? option.fn(this) : option.inverse(this)
   }
