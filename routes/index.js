1f40bacff557e18665cf301e65de5acb49547632
playcsgo <sakura_0125@hotmail.com>
2024-03-18 16:49:20 +0800

    feat: add-and-delete-comment

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
+
 const restController = require('../controllers/restaurant-controller.js')
 const userController = require('../controllers/user-controler')
+const commentController = require('../controllers/comment-controller')
+
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 const { authenticated } = require('../middleware/auth')
@@ -15,6 +18,9 @@ router.post('/signup', userController.signUp)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
+router.post('/comments', authenticated, commentController.postComment)
 
 
 router.get('/signin', userController.signInPage)
